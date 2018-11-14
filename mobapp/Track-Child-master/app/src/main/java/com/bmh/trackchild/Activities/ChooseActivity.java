71e27651990a260d58bf56cc0386a47a0bebec40
dmdghto <dmdghto@gmail.com>
2018-11-14 11:00:21 +0900

    intergration

--- mobapp/Track-Child-master/app/src/main/java/com/bmh/trackchild/Activities/ChooseActivity.java
+++ mobapp/Track-Child-master/app/src/main/java/com/bmh/trackchild/Activities/ChooseActivity.java
@@ -33,6 +33,7 @@ public class ChooseActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose);
         swc1 = (Switch) findViewById(R.id.switch1);
+        swc2 = (Switch) findViewById(R.id.switch2);
         swc3 = (Switch) findViewById(R.id.switch3);
 
 
@@ -57,6 +58,31 @@ public class ChooseActivity extends AppCompatActivity{
                 else{
                     Toast.makeText(getApplicationContext(),"off",Toast.LENGTH_SHORT).show();
 
+                }
+            }
+        });
+
+        swc2.setOnCheckedChangeListener(new Switch.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                String str = String.valueOf(isChecked);
+                // Toast.makeText(getApplicationContext(),"체크상태 = "+isChecked,Toast.LENGTH_SHORT);
+
+                if (isChecked) {
+                    Toast.makeText(getApplicationContext(), "on", Toast.LENGTH_SHORT).show();
+                    Log.d("test", "on");
+                    Intent intent = new Intent(
+                            getApplicationContext(),//현재제어권자
+                            BatteryService.class); // 이동할 컴포넌트
+                    startService(intent); // 서비스 시작
+
+
+                } else {
+                    Toast.makeText(getApplicationContext(), "off", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(
+                            getApplicationContext(),//현재제어권자
+                            BatteryService.class); // 이동할 컴포넌트
+                    stopService(intent); // 서비스 종료
                 }
             }
         });
